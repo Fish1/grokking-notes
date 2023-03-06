acde06c64cdddb155b1d26810c0fe176b5a4735a
Jacob Enders <>
2023-03-05 23:22:02 -0500

    finish graphs

--- chapter-6/graph.py
+++ chapter-6/graph.py
@@ -1,6 +1,5 @@
 from collections import deque
 
-search_queue = deque()
 
 graph = {}
 graph["you"] = ["alice", "bob", "claire"]
@@ -8,8 +7,34 @@ graph["bob"] = ["anuj", "peggy"]
 graph["alice"] = ["peggy"]
 graph["claire"] = ["thom", "jonny"]
 graph["anuj"] = []
-graph["peggy"] = []
+graph["peggy"] = ["bob"]
 graph["thom"] = []
 graph["jonny"] = []
 
-def iterate_over_graph(graph, search_queue):
+
+def person_is_seller(name):
+    return name[-1] == 'm'
+
+def search(name):
+    search_queue = deque()
+    search_queue += graph[name]
+
+    searched = {}
+
+    # while the search queue is not empty
+    while search_queue:
+        # pop the first person off the queue
+        person = search_queue.popleft()
+        if person in searched:
+            continue
+        # check if person is a mango seller
+        if person_is_seller(person):
+            # if they are a seller, print their name, and exit
+            print(person + " is a mango seller!")
+            return
+        else:
+            # otherwise, add all of their friends to the search queue
+            searched[person] = True
+            search_queue += graph[person]
+
+search("you")
